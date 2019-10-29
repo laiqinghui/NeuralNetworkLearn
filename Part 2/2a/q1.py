part 1 fixed scaling

Part 2/2a/q1.py
@@ -14,7 +14,7 @@ NUM_CLASSES = 10
 IMG_SIZE = 32
 NUM_CHANNELS = 3
 learning_rate = 0.001
-epochs = 500
+epochs = 1000
 batch_size = 128
 
 seed = 10
@@ -94,6 +94,7 @@ def main():
     print(testX.shape, testY.shape)
 
     trainX = (trainX - np.min(trainX, axis=0)) / np.max(trainX, axis=0)
+    testX = (testX - np.min(testX, axis=0)) / np.max(testX, axis=0)
 
     # Create the model
     x = tf.placeholder(tf.float32, [None, IMG_SIZE * IMG_SIZE * NUM_CHANNELS])
@@ -145,7 +146,7 @@ def main():
     plt.xlabel('epochs')
     plt.ylabel('loss')
     plt.title('Training Loss')
-    plt.savefig('loss.png')
+    plt.savefig('q1figs/loss.png')
 
     plt.figure(2)
     plt.plot(range(epochs), test_acc, 'g', label='Test Accuracy')
@@ -153,7 +154,7 @@ def main():
     plt.xlabel('epochs')
     plt.ylabel('loss')
     plt.title('Test Accuracy')
-    plt.savefig('loss.png')
+    plt.savefig('q1figs/loss.png')
 
     plt.figure(3)
     plt.gray()
@@ -162,7 +163,7 @@ def main():
         plt.subplot(4, 6, i + 1);
         plt.axis('off');
         plt.imshow(_conv_1[0, :, :, i])
-    plt.savefig('feature_map.png')
+    plt.savefig('q1figs/feature_map.png')
 
     plt.figure(4)
     plt.gray()
@@ -171,7 +172,7 @@ def main():
         plt.subplot(4, 3, i + 1);
         plt.axis('off');
         plt.imshow(_conv_1[0, :, :, i])
-    plt.savefig('pool.png')
+    plt.savefig('q1figs/pool.png')
 
     # plt.figure()
     # plt.gray()

Part 2/2a/q2.py
@@ -14,7 +14,7 @@ NUM_CLASSES = 10
 IMG_SIZE = 32
 NUM_CHANNELS = 3
 learning_rate = 0.001
-epochs = 500
+epochs = 10000
 batch_size = 128
 
 seed = 10
@@ -142,7 +142,7 @@ def buildandrunmodel(trainX, trainY, testX, testY, c1_kernel, c2_kernel):
     plt.xlabel('epochs')
     plt.ylabel('loss')
     plt.title('Training Loss for ' + title)
-    plt.savefig('q2figs/Training Loss for ' + title + '.png')
+    plt.savefig('q2figs10000/Training Loss for ' + title + '.png')
 
     plt.figure(2)
     plt.clf()
@@ -151,7 +151,7 @@ def buildandrunmodel(trainX, trainY, testX, testY, c1_kernel, c2_kernel):
     plt.xlabel('epochs')
     plt.ylabel('Accuracy')
     plt.title('Test Accuracy for ' + title)
-    plt.savefig('q2figs/Test Accuracy for ' + title + '.png')
+    plt.savefig('q2figs10000/Test Accuracy for ' + title + '.png')
 
 
 def main():
@@ -163,12 +163,23 @@ def main():
     print(testX.shape, testY.shape)
 
     trainX = (trainX - np.min(trainX, axis=0)) / np.max(trainX, axis=0)
+    testX = (testX - np.min(testX, axis=0)) / np.max(testX, axis=0)
 
-    for c1 in range(20, 70, 10):
-        for c2 in range(20, 70, 10):
+    # kernels = [(30,170), (40,40), (40,90), (60,60), (70,40), (70,140),
+    #            (70,160), (70,190), (80,30), (80,80), (80,100), (90,130),
+    #            (100,40), (100,170), (110,30),( 110,120), (120,30),(120,170),
+    #            (130,30), (140,20), (140,150), (150,80), (150,80), (150,150),
+    #            (160,60), (160,60), (160,70), (180,60), (190,180),]
+
+    for c1 in range(20, 150, 10):
+        for c2 in range(20, 150, 10):
             print("C1 size: ", c1, " and C2 size: ", c2)
             buildandrunmodel(trainX, trainY, testX, testY, c1, c2)
             print("=====================================================================")
+
+    # for kernel in kernels:
+    #     buildandrunmodel(trainX, trainY, testX, testY, kernel[0], kernel[1])
+    #     print("=====================================================================")
 
 
 

Part 2/2a/q3.py
@@ -94,6 +94,7 @@ def main():
     print(testX.shape, testY.shape)
 
     trainX = (trainX - np.min(trainX, axis=0)) / np.max(trainX, axis=0)
+    testX = (testX - np.min(testX, axis=0)) / np.max(testX, axis=0)
 
     # Create the model
     x = tf.placeholder(tf.float32, [None, IMG_SIZE * IMG_SIZE * NUM_CHANNELS])
@@ -144,7 +145,7 @@ def main():
         plt.xlabel('Epochs')
         plt.ylabel('Accuracy')
         plt.subplot(1, 2, 2)
-        plt.plot(range(1, epochs, 10), loss_list, label='Training Loss')
+        plt.plot(range(epochs), loss_list, label='Training Loss')
         plt.xlabel('Epochs')
         plt.ylabel('Loss')
         plt.title('Adding the momentum term with momentum 𝛾=0.1')
@@ -179,7 +180,7 @@ def main():
         plt.xlabel('Epochs')
         plt.ylabel('Accuracy')
         plt.subplot(1, 2, 2)
-        plt.plot(range(1, epochs, 10), loss_list, label='Training Loss')
+        plt.plot(range(epochs), loss_list, label='Training Loss')
         plt.xlabel('Epochs')
         plt.ylabel('Loss')
         plt.title('Using RMSProp algorithm for learning')
@@ -214,7 +215,7 @@ def main():
         plt.xlabel('Epochs')
         plt.ylabel('Accuracy')
         plt.subplot(1, 2, 2)
-        plt.plot(range(1, epochs, 10), loss_list, label='Training Loss')
+        plt.plot(range(epochs), loss_list, label='Training Loss')
         plt.xlabel('Epochs')
         plt.ylabel('Loss')
         plt.title('Using Adam optimizer for learning')

Part 2/2a/q3_dropout.py
@@ -88,7 +88,7 @@ def cnn(images, c1_kernel=9, c2_kernel=5):
     b4 = tf.Variable(tf.zeros([10]), name='biases_4')
     logits = tf.matmul(fc1_drop, W4) + b4
 
-    return logits
+    return logits, keep_prob
 
 
 def main():
@@ -99,12 +99,13 @@ def main():
     print(testX.shape, testY.shape)
 
     trainX = (trainX - np.min(trainX, axis=0)) / np.max(trainX, axis=0)
+    testX = (testX - np.min(testX, axis=0)) / np.max(testX, axis=0)
 
     # Create the model
     x = tf.placeholder(tf.float32, [None, IMG_SIZE * IMG_SIZE * NUM_CHANNELS])
     y_ = tf.placeholder(tf.float32, [None, NUM_CLASSES])
 
-    logits = cnn(x)
+    logits, keep_prob = cnn(x)
 
     cross_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(labels=y_, logits=logits)
     loss = tf.reduce_mean(cross_entropy)
@@ -137,6 +138,7 @@ def main():
                 _, loss_ = sess.run([train_step1, loss], {x: trainX[start:end], y_: trainY[start:end], keep_prob: 0.5})
                 loss_temp.append(loss_)
 
+
             loss_list.append(np.mean(loss_temp))
             test_acc.append(accuracy.eval(feed_dict={x: testX, y_: testY, keep_prob: 1.0}))
             loss_temp = []
@@ -149,7 +151,7 @@ def main():
         plt.xlabel('Epochs')
         plt.ylabel('Accuracy')
         plt.subplot(1, 2, 2)
-        plt.plot(range(1, epochs), loss_list, label='Training Loss')
+        plt.plot(range(epochs), loss_list, label='Training Loss')
         plt.xlabel('Epochs')
         plt.ylabel('Loss')
         plt.title('Adding the momentum term with momentum 𝛾=0.1')
@@ -184,7 +186,7 @@ def main():
         plt.xlabel('Epochs')
         plt.ylabel('Accuracy')
         plt.subplot(1, 2, 2)
-        plt.plot(range(1, epochs), loss_list, label='Training Loss')
+        plt.plot(range(epochs), loss_list, label='Training Loss')
         plt.xlabel('Epochs')
         plt.ylabel('Loss')
         plt.title('Using RMSProp algorithm for learning')
@@ -219,7 +221,7 @@ def main():
         plt.xlabel('Epochs')
         plt.ylabel('Accuracy')
         plt.subplot(1, 2, 2)
-        plt.plot(range(1, epochs), loss_list, label='Training Loss')
+        plt.plot(range(epochs), loss_list, label='Training Loss')
         plt.xlabel('Epochs')
         plt.ylabel('Loss')
         plt.title('Using Adam optimizer for learning')

Part 2/2a/start_project_2a.py
@@ -69,12 +69,36 @@ def cnn(images):
 def main():
 
     trainX, trainY = load_data('data_batch_1')
-    print(trainX.shape, trainY.shape)
+    print("trainX: ")
+    print(trainX)
+    print("trainX.shape: ", trainX.shape)
+    print("trainY: ")
+    print(trainY)
+    print("trainY.shape: ", trainY.shape)
+
     
     testX, testY = load_data('test_batch_trim')
-    print(testX.shape, testY.shape)
+    print("testX: ")
+    print(testX)
+    print("testX.shape: ", testX.shape)
+    print("testY: ")
+    print(testY)
+    print("testY.shape: ", testY.shape)
 
-    trainX = (trainX - np.min(trainX, axis = 0))/np.max(trainX, axis = 0)
+    trainX_default = (trainX - np.min(trainX, axis = 0))/np.max(trainX, axis = 0)
+    trainX = (trainX - np.min(trainX, axis=0)) / (np.max(trainX, axis=0) - np.min(trainX, axis=0))
+    testX = (testX - np.min(testX, axis=0)) / (np.max(testX, axis=0) - np.min(testX, axis=0))
+
+    print("np.min(trainX, axis=0)", np.min(trainX, axis=0))
+
+    print("trainX_default_normalized: ")
+    print(trainX_default)
+
+    print("trainX_normalized: ")
+    print(trainX)
+    print("testX_normalized: ")
+    print(testX)
+
 
     # Create the model
     x = tf.placeholder(tf.float32, [None, IMG_SIZE*IMG_SIZE*NUM_CHANNELS])